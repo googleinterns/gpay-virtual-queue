2712b9d34c6de5297e3ca2f39bd51be6e38a8a88
ikoder <ikoder@google.com>
2020-06-10 20:59:31 +0530

    added in-memory shop respository

---
+++ backendservice/src/main/java/com/google/gpay/virtualqueue/backendservice/repository/InMemoryShopRepository.java
@@ -0,0 +1,50 @@
+/*
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.google.gpay.virtualqueue.backendservice.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.google.gpay.virtualqueue.backendservice.model.Shop;
+import com.google.gpay.virtualqueue.backendservice.model.ShopOwner;
+import com.google.gpay.virtualqueue.backendservice.model.Token;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.Getter;
+
+@Repository
+@Getter
+public class InMemoryShopRepository implements VirtualQueueRepository {
+	private Map<UUID, Token> token_mappings = new HashMap<>();
+	private Map<UUID, Shop> shop_mappings = new HashMap<>();
+	private Map<UUID, List<Token>> tokens;
+	private Map<UUID, AtomicInteger> lastAllottedToken;
+	private Map<String, ShopOwner> shopOwner_mappings;
+
+	public UUID createShop(Shop shop) {
+		UUID uuid = UUID.randomUUID();
+		shop.setShopId(uuid);
+		shop_mappings.put(uuid, shop);
+		AtomicInteger lastToken = new AtomicInteger(0);
+		lastAllottedToken.put(uuid, lastToken);
+		return shop.getShopId();
+	}
+}

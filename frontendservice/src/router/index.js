35c00cd151391334ff13f6868a16a7cda9184676
KarishmaSinghChauhan <kschauhan@google.com>
2020-06-22 02:24:00 +0530

    remove ToDo from index.js

--- frontendservice/src/router/index.js
+++ frontendservice/src/router/index.js
@@ -15,8 +15,6 @@ import firebase from "firebase";
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-//ToDo: kschauhan - import vue components, edit paths and router when more .vue files are pushed
-
 import About from "@/views/About";
 import CustomerHome from "@/views/CustomerHome";
 import ShopOwnerHome from "@/views/ShopOwnerHome";

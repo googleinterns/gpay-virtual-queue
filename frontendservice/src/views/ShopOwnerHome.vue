742c55e4a77008a76fde6b6d81a0d661f0f0f072
ikoder <ikoder@google.com>
2020-06-22 00:00:50 +0530

    Add shop owner home feature

--- frontendservice/src/views/ShopOwnerHome.vue
+++ frontendservice/src/views/ShopOwnerHome.vue
@@ -1,79 +1,171 @@
-/* Copyright 2020 Google LLC Licensed under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance with the License. You
-may obtain a copy of the License at https://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software distributed
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License. */
+/*
+Copyright 2020 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 <template>
-  <div class="home">
-    <h1 v-if="isLoggedIn">Welcome! {{ name }}</h1>
-    <p>
-      This page will be Home page for the shop-owners. 
-    </p>
-    <p>
-      ToDo: Implement ShopOwnerHome page features including NavBar, Search Bar, MyShops and AddShop.
-    </p> 
+  <div>
     <div v-if="isVerified">
-      <router-link to="/create-shop">Create new shop</router-link>
-    </div>
-    <div v-if="isVerified">
-      <router-link to="/my-shops">My Shops</router-link>
+      <shopOwnerNavbar />
+      <h1 id="heading" v-if="isVerified">My Shops</h1>
+      <input type="search" v-model="searchResult" placeholder="search shops" id="searchbar" />
+      <table class="table">
+        <thead>
+          <th>Shop Name</th>
+          <th>Shop Type</th>
+          <th>All Tokens of Shop</th>
+          <th>Delete</th>
+        </thead>
+        <tbody>
+          <tr v-for="shop in filteredShops" :key="shop['shop'].shopId">
+            <td>
+              <a v-bind:href="'/shop-owner/shops/'+ shop['shop'].shopId" :shopId="shop['shop'].shopId">{{shop['shop'].shopName}}</a>
+            </td>
+            <td>{{shop['shop'].shopType}}</td>
+            <td v-if="shop['shop'].status=='ACTIVE'">
+              <a v-bind:href="'/shop-owner/shops/'+ shop.shopId" :id="shop.shopId">{{shop.numberOfActiveTokens}}</a>
+            </td>
+            <td v-if="shop['shop'].status!='ACTIVE'">
+              <a v-bind:href="'/shop-owner/shops/'+ shop.shopId" :id="shop.shopId">0</a>
+            </td>
+            <td v-if="shop['shop'].status=='ACTIVE'">
+              <button @click="deleteshop(shop['shop'].shopId)">x</button>
+            </td>
+            <td v-if="shop['shop'].status!='ACTIVE'">
+              <button>restore</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
     <div v-if="isLoggedIn && !isVerified">
-      <router-link to="/verify">Verify Me!</router-link>
+      <router-link to="/verify"><button class="button is-primary">Verify Me!</button></router-link>
     </div>
-    <div v-if="!isLoggedIn">
-      <router-link to="/about">Back</router-link>
-    </div>
-    <br />
-    <a class="button is-danger" v-if="isLoggedIn" @click="logout">Logout</a>
   </div>
 </template>
 
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 <script>
+import shopOwnerNavbar from "../components/NavBar-ShopOwner";
+import axios from "axios";
+import Swal from "sweetalert2";
 import firebase from "firebase";
-
 export default {
-  name: "shopOwnerHome",
-
+  name: "MyShops",
+  components: {
+    shopOwnerNavbar
+  },
   data() {
     return {
-      name: "",
-      uid: "",
+      shopList: [],
+      searchResult: "",
+      renderComponent: true,
       isLoggedIn: false,
-      isVerified: false,
+      isVerified: false
     };
+  },
+  methods: {
+    deleteshop: function(id) {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then(result => {
+        if (result.isConfirmed == true) {
+          axios
+            .put("http://penguin.termina.linux.test:8080/shop/", {
+              shopId: id,
+              shopStatus: "DELETED"
+            })
+            .then();
+          this.renderComponent = false;
+          var self = this;
+          this.$nextTick(() => {
+            axios
+              .get(
+                "http://penguin.termina.linux.test:8080/shop/" +
+                  firebase.auth().currentUser.uid
+              )
+              .then(response => (self.shopList = response.data.shops));
+            self.renderComponent = true;
+          });
+        }
+      });
+    }
   },
   created() {
     var user = firebase.auth().currentUser;
     if (user) {
-      this.name = user.displayName;
-      this.uid = user.uid;
       this.isLoggedIn = true;
       if (user.emailVerified) {
         this.isVerified = true;
       }
     }
+    var self = this;
+    setInterval(function() {
+      axios
+        .get(
+          "http://penguin.termina.linux.test:8080/shop/" +
+            firebase.auth().currentUser.uid
+        )
+        .then(response => (self.shopList = response.data.shops));
+    }, 2000);
   },
-  methods: {
-    logout: function() {
-      firebase
-        .auth()
-        .signOut()
-        .then(() => {
-          this.$router.replace("login");
-        });
-    },
-  },
+  computed: {
+    filteredShops: function() {
+      return this.shopList.filter(shop => {
+        var result = this.searchResult.toLowerCase();
+        var shopName = shop["shop"].shopName.toLowerCase();
+        return shopName.match(result);
+      });
+    }
+  }
 };
 </script>
 
 <style scoped>
-button {
-  margin-top: 1%;
-  width: 10%;
+* {
+  text-align: center;
+}
+button{
+  margin: 2%;
+}
+.control {
+  text-align: center;
+}
+#heading {
+  font-size: 2rem;
+}
+#search {
+  width: 50%;
+  line-height: 2rem;
+}
+#searchbar {
+  border-width: 0.2em;
+  border-radius: 0.7em;
+  width: 40rem;
+  line-height: 2em;
+}
+#searchbar {
+  margin-left: 2%;
+}
+.table {
+  width: 100%;
+}
+.table tbody tr td button:hover {
   cursor: pointer;
+  background-color: aquamarine;
 }
 </style>

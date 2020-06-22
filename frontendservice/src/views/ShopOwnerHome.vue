d0164ad3c69423822a796b8f8f2b5f96a7634763
ikoder <ikoder@google.com>
2020-06-22 19:38:57 +0530

    Removed revert message

--- frontendservice/src/views/ShopOwnerHome.vue
+++ frontendservice/src/views/ShopOwnerHome.vue
@@ -77,7 +77,7 @@ export default {
     deleteshop: function(id) {
       Swal.fire({
         title: "Are you sure?",
-        text: "You won't be able to revert this!",
+        text: "",
         icon: "warning",
         showCancelButton: true,
         confirmButtonColor: "#3085d6",

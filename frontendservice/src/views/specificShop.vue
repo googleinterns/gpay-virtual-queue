8ae3f19b2db89022241f0c50ea0df35bfbeb57a1
bavanya <bavanya@google.com>
2020-06-24 10:37:12 +0530

    removed minor nits

--- frontendservice/src/views/specificShop.vue
+++ frontendservice/src/views/specificShop.vue
@@ -136,7 +136,7 @@ export default {
                   Cookie.set("tokenid", JSON.stringify(self.allCookieList));
                   self.flag = false;
                   self.statusFlag = true;
-                } //TODO: change the value of tokenStatus flag if the token was deleted by the shop owner.
+                }
               }
             });
           }
@@ -188,7 +188,7 @@ export default {
 
   created() {
     this.getShopInfo();
-    this.timer = setInterval(this.getShopInfo, 0.1 /*1 second*/);
+    this.timer = setInterval(this.getShopInfo, 1 /*1 second*/);
   }
 };
 </script>
@@ -228,8 +228,4 @@ h1 {
 .table tbody tr td {
   text-align: center;
 }
-
-/*      <div v-if="statusFlag">
-        <statusUpdate></statusUpdate>
-      </div> */
 </style>
